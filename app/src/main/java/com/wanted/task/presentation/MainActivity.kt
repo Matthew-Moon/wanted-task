de37ec679b86a7fc23e980a9ffc215499d09b22a
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-05-25 04:54:01 +0900

    feat: 불 필요한 코드 제거 및 CompanyListScreen 연결

--- app/src/main/java/com/wanted/task/presentation/MainActivity.kt
+++ app/src/main/java/com/wanted/task/presentation/MainActivity.kt
@@ -3,45 +3,25 @@ package com.wanted.task.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.wanted.task.presentation.list.CompanyListScreen
 import com.wanted.task.presentation.theme.WantedTaskTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+
         setContent {
             WantedTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CompanyListScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WantedTaskTheme {
-        Greeting("Android")
     }
 }
