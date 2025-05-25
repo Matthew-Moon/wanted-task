bc7e25d985339f0f077899dabd6dcae9e3d6090b
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-05-25 23:09:23 +0900

    feat: 하이퍼링크 텍스트 컴포넌트 개발

---
+++ app/src/main/java/com/wanted/task/presentation/detail/HyperlinkText.kt
@@ -0,0 +1,45 @@
+package com.wanted.task.presentation.detail
+
+import android.content.Intent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+
+@Composable
+fun HyperlinkText(
+    modifier: Modifier,
+    title: String,
+    url: String
+) {
+    val context = LocalContext.current
+
+    Column {
+        Text(
+            modifier = modifier.padding(top = 24.dp),
+            text = title
+        )
+        Text(
+            modifier = modifier.clickable {
+                val finalUrl =
+                    if (url.startsWith("www.")) "https://$url"
+                    else url
+
+                val intent = Intent(Intent.ACTION_VIEW, finalUrl.toUri())
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(intent, null)
+            },
+            text = url,
+            color = Color.Blue,
+            textDecoration = TextDecoration.Underline,
+        )
+
+    }
+}
