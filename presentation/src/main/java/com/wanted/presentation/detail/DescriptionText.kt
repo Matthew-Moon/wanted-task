9c44c5dda7c65c3eaa68ef4c86977d97b7cbabaf
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-05-27 22:52:07 +0900

    chore: Warning 제거 및 미사용 import 제거

--- presentation/src/main/java/com/wanted/presentation/detail/DescriptionText.kt
+++ presentation/src/main/java/com/wanted/presentation/detail/DescriptionText.kt
@@ -1,9 +1,7 @@
 package com.wanted.presentation.detail
 
 import android.content.Intent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,7 +9,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
@@ -20,7 +17,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
-import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 
 @Composable
